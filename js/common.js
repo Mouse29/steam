ccf938d2e8384aada7d0324b6aece573fa2bf136
Сергей <Veremey@i.ua>
2014-10-18 17:19:17 +0300

    `12

--- js/common.js
+++ js/common.js
@@ -1,26 +1,7 @@
 $(document).ready(function() {
-	// $('.js-open').children().toggleClass('icon-arrow-down')
-
-
-	$('.js-open').mouseover(function() {
-		$(this).next('.submenu').slideDown(300).and().slideUp(300)
-	})
-		$(".js-open").onclick(function() {
-			$("icon-arrow-left").removeClass("icon-arrow-left")
-	})
-		// $(this).children().removeClass("icon-arrow-left").addClass("icon-arrow-down")
-
-
+	$('.js-open').hover(function() {
+			$(this).children().removeClass('icon-arrow-left').addClass('icon-arrow-down')
+		}, function(){
+			$(this).children().removeClass('icon-arrow-down').addClass('icon-arrow-left')
+		});
 });
-
-
-
-// 	var sprach = $('.content').addClass("is-active");
-
-// 	$('.title').click(function(){
-// 		sprach.slideUp();
-// 		$(this).next().slideDown();
-
-// 	})
-
-// });
